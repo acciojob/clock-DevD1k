23116a8ee62dcedd1421ce7dfe1a7b469d387ec5
DevD1k <113674752+DevD1k@users.noreply.github.com>
2023-04-24 20:23:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function updateClock() {
+			const now = new Date();
+			const date = now.toLocaleDateString();
+			const time = now.toLocaleTimeString();
+			document.getElementById('timer').innerHTML = `${date} ${time}`;
+		}
+
+		// call the function every second to update the time
+		setInterval(updateClock, 1000);//your JS code here. If required.
+function updateClock() {
+			const now = new Date();
+			const date = now.toLocaleDateString();
+			const time = now.toLocaleTimeString();
+			document.getElementById('timer').innerHTML = `${date} ${time}`;
+		}
+
+		// call the function every second to update the time
+		setInterval(updateClock, 1000);
